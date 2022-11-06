cff059fa830b58050a854d4dbc05e72bc0804ed0
WajeehHussein <wajehessam3@gmail.com>
2022-11-06 02:36:56 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,71 @@
-import './App.scss'
-import {Home, Profile} from "./pages";
-import { createBrowserRouter,RouterProvider,Outlet} from 'react-router-dom'
-import { LeftBar,Navbar, RightBar } from './components';
-import { useContext } from 'react';
-import { DarkModeContext } from './context/darkModeContext';
-import { AuthContext } from './context/authContext';
+import {createBrowserRouter,RouterProvider,Outlet,Navigate} from "react-router-dom";
+import {Navbar,LeftBar ,RightBar } from "./components";
+import {Home, Profile,Login,Register} from "./pages";
+
+import "./App.scss";
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
+import { AuthContext } from "./context/authContext";
+
 function App() {
-  const {currentUser} =useContext(AuthContext);
-  const {darkMode} = useContext(DarkModeContext);
+  const {currentUser} = useContext(AuthContext);
+
+  const { darkMode } = useContext(DarkModeContext);
 
   const Layout = () => {
-    return(
-      <div className={`theme-${darkMode ? 'dark' : 'light'}`}>
+    return (
+      <div className={`theme-${darkMode ? "dark" : "light"}`}>
         <Navbar />
-        <div style={{display:"flex"}}>
+        <div style={{ display: "flex" }}>
           <LeftBar />
-          <div style={{flex:6}}>
-          <Outlet />
+          <div style={{ flex: 6 }}>
+            <Outlet />
           </div>
           <RightBar />
         </div>
       </div>
     );
   };
-  
-  const ProtectedRoute = ({children}) =>{
-    if(!currentUser) {
-      return children;
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/" />;
     }
+
     return children;
-  }
+  };
 
   const router = createBrowserRouter([
     {
-      path:'/',
+      path: "/",
       element: (
-      <ProtectedRoute>
-        <Layout />
-      </ProtectedRoute>),
-      children:[
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
         {
-          path:"/*",
-          element: <Home />
+          path: "/",
+          element: <Home />,
         },
         {
-          path:"/profile/:id",
-          element: <Profile />
-        }
-      ]
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
     },
-
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
   ]);
+
   return (
-    <div className="App">
+    <div>
       <RouterProvider router={router} />
     </div>
   );
